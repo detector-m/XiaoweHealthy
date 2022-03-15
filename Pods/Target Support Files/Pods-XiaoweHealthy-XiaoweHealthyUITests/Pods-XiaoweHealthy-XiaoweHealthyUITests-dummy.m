1128573a5e3a2d9b573950ac4bafd8f4c2e82e56
Riven <lrw90316@163.com>
2022-03-15 18:57:27 +0800

    创建 Pod

---
+++ Pods/Target Support Files/Pods-XiaoweHealthy-XiaoweHealthyUITests/Pods-XiaoweHealthy-XiaoweHealthyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XiaoweHealthy_XiaoweHealthyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_XiaoweHealthy_XiaoweHealthyUITests
+@end
---
+++ Pods/Target Support Files/Pods-XiaoweHealthy-XiaoweHealthyUITests/Pods-XiaoweHealthy-XiaoweHealthyUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_XiaoweHealthy_XiaoweHealthyUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_XiaoweHealthy_XiaoweHealthyUITestsVersionString[];
+
