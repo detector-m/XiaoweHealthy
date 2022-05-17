4fccc4c68296c91cc77a79e07418fc434168727b
Riven <lrw90316@163.com>
2022-05-17 13:33:19 +0800

    修改数据解析方式

--- XiaoweHealthy/Healthy/ChartDataHandler/XWHHealthyChartDataHandler.swift
+++ XiaoweHealthy/Healthy/ChartDataHandler/XWHHealthyChartDataHandler.swift
@@ -9,8 +9,17 @@ import Foundation
 //import AAInfographics
 
 
+typealias XWHChartDataParseResult<ChartValue, RawValue> = (xAxisValue: String, yValue: ChartValue, rawValue: RawValue?)
+typealias XWHChartDataParseHandler<ChartValue, RawValue> = (_ dateType: XWHHealthyDateSegmentType, _ i: Int, _ iDate: Date, _ rawItems: [RawValue]) -> XWHChartDataParseResult<ChartValue, RawValue>
+
+typealias XWHChartDataYAxisResult<ChartValue> = (max: Double, granularity: Double, yAxisValues: [String])
+typealias XWHChartDataYAxisHandler<ChartValue> = (_ yValues: [ChartValue], _ yAxisLabelCount: Int) -> XWHChartDataYAxisResult<ChartValue>
+
+
 /// 图表数据管理器
 class XWHHealthyChartDataHandler {
+    
+    #if false
     
 //    class func getSleepDayChartData(_ sleepUIModel: XWHHealthySleepUISleepModel) -> [AASeriesElement] {
 //        let zeroArray = [0, 0]
@@ -154,6 +163,172 @@ class XWHHealthyChartDataHandler {
         return retModel
     }
     
+    #endif
+    
+    class func getSleepWeekMonthYearChartDataModel(date: Date, dateType: XWHHealthyDateSegmentType, sItems: [XWHHealthySleepUISleepItemModel]) -> XWHSleepWMYChartDataModel {
+        let retModel = XWHSleepWMYChartDataModel()
+        
+        var iXAxisValue = ""
+        var iRawValue: XWHHealthySleepUISleepItemModel? = nil
+        let defaultIYValue: [Double] = [0, 0, 0]
+        var iYValue = defaultIYValue
+        
+        configChartDataModel(date: date, dateType: dateType, rawItems: sItems, chartDataModel: retModel, dayParser: { (dateType, i, iDate, rawItems) -> (String, [Double], XWHHealthySleepUISleepItemModel?) in
+            iXAxisValue = ""
+            iYValue = defaultIYValue
+            iRawValue = nil
+            
+            let iDateString = iDate.string(withFormat: XWHDate.standardYearMonthDayFormat)
+//            if let iItem = sItems.first(where: { $0.timeAxis == iDateString }) {
+//                iYValue = [iItem.deepSleepDuration.double, iItem.lightSleepDuration.double, iItem.awakeDuration.double]
+//
+//                iRawValue = iItem
+//            }
+            
+            (iYValue, iRawValue) = getChartRawValue(in: sItems, where: { $0.timeAxis == iDateString }, chartValueHandler: { [$0.deepSleepDuration.double, $0.lightSleepDuration.double, $0.awakeDuration.double] }, defaultChartValue: defaultIYValue)
+            iXAxisValue = getDayXAxisValue(i: i, iDate: iDate)
+            
+            return (iXAxisValue, iYValue, iRawValue)
+        }, weekParser: { dateType, i, iDate, rawItems in
+            iXAxisValue = ""
+            iYValue = defaultIYValue
+            iRawValue = nil
+            
+            let iDateString = iDate.string(withFormat: XWHDate.standardYearMonthDayFormat)
+//            if let iItem = sItems.first(where: { $0.timeAxis == iDateString }) {
+//                iYValue = [iItem.deepSleepDuration.double, iItem.lightSleepDuration.double, iItem.awakeDuration.double]
+//
+//                iRawValue = iItem
+//            }
+            
+            (iYValue, iRawValue) = getChartRawValue(in: sItems, where: { $0.timeAxis == iDateString }, chartValueHandler: { [$0.deepSleepDuration.double, $0.lightSleepDuration.double, $0.awakeDuration.double] }, defaultChartValue: defaultIYValue)
+            
+            iXAxisValue = getWeekXAxisValue(i: i, iDate: iDate)
+
+            return (iXAxisValue, iYValue, iRawValue)
+        }, monthParser: { dateType, i, iDate, rawItems in
+            iXAxisValue = ""
+            iYValue = defaultIYValue
+            iRawValue = nil
+            
+            let iDateString = iDate.string(withFormat: XWHDate.standardYearMonthDayFormat)
+//            if let iItem = sItems.first(where: { $0.timeAxis == iDateString }) {
+//                iYValue = [iItem.deepSleepDuration.double, iItem.lightSleepDuration.double, iItem.awakeDuration.double]
+//
+//                iRawValue = iItem
+//            }
+            
+            (iYValue, iRawValue) = getChartRawValue(in: sItems, where: { $0.timeAxis == iDateString }, chartValueHandler: { [$0.deepSleepDuration.double, $0.lightSleepDuration.double, $0.awakeDuration.double] }, defaultChartValue: defaultIYValue)
+            
+            iXAxisValue = getMonthXAxisValue(i: i, iDate: iDate)
+
+            return (iXAxisValue, iYValue, iRawValue)
+        }, yearParser: { dateType, i, iDate, rawItems in
+            iXAxisValue = ""
+            iYValue = defaultIYValue
+            iRawValue = nil
+            
+//            if let iItem = sItems.first(where: { $0.timeAxis.date(withFormat: XWHDate.standardYearMonthDayFormat)?.monthBegin == iDate }) {
+//                iYValue = [iItem.deepSleepDuration.double, iItem.lightSleepDuration.double, iItem.awakeDuration.double]
+//                iRawValue = iItem
+//            }
+            
+            (iYValue, iRawValue) = getChartRawValue(in: sItems, where: { $0.timeAxis.date(withFormat: XWHDate.standardYearMonthDayFormat)?.monthBegin == iDate }, chartValueHandler: { [$0.deepSleepDuration.double, $0.lightSleepDuration.double, $0.awakeDuration.double] }, defaultChartValue: defaultIYValue)
+            
+            iXAxisValue = getYearXAxisValue(i: i, iDate: iDate)
+
+            return (iXAxisValue, iYValue, iRawValue)
+        }, yAxisHandler: { yValues, yAxisLabelCount in
+            let yAxisCount: Double = yAxisLabelCount.double
+            var max = yValues.map({ $0.sum() }).max() ?? 0
+            var hMax = ceil(max / 60)
+            let hGranularity = ceil(hMax / yAxisCount)
+            
+            hMax = hGranularity * yAxisCount
+            max = hMax * 60
+            let granularity = hGranularity * 60
+            
+            return (max, granularity, [])
+        }, yAxisLabelCount: 5)
+        
+        return retModel
+    }
+    
+    class func getChartRawValue<ChartValue, RawValue>(in rawValues: [RawValue], where predicate: (RawValue) -> Bool, chartValueHandler: (RawValue) -> ChartValue, defaultChartValue: ChartValue) -> (ChartValue, RawValue?) {
+        guard let retRawValue = rawValues.first(where: predicate) else {
+            return (defaultChartValue, nil)
+        }
+        
+        let cValue = chartValueHandler(retRawValue)
+        
+        return (cValue, retRawValue)
+    }
+    
+    @discardableResult
+    class func configChartDataModel<ChartValue, RawValue>(date: Date, dateType: XWHHealthyDateSegmentType, rawItems: [RawValue], chartDataModel: XWHChartDataBaseModel, dayParser: XWHChartDataParseHandler<ChartValue, RawValue>, weekParser: XWHChartDataParseHandler<ChartValue, RawValue>, monthParser: XWHChartDataParseHandler<ChartValue, RawValue>, yearParser: XWHChartDataParseHandler<ChartValue, RawValue>, yAxisHandler: XWHChartDataYAxisHandler<ChartValue>, yAxisLabelCount: Int = 5) -> XWHChartDataBaseModel {
+        let retModel = chartDataModel
+        var xAxisValues = [String]()
+//        var yAxisValues = [String]()
+        var yValues = [ChartValue]()
+        var rawValues = [RawValue?]()
+
+        let bDates = getBeginDates(date: date, dateType: dateType)
+        let xLabelCount = bDates.count
+
+        var iXAxisValue = ""
+        var iRawValue: RawValue? = nil
+        var iYValue: ChartValue
+
+        switch dateType {
+        case .day:
+            return retModel
+
+        case .week:
+//            xLabelCount = 7
+
+            for (i, iDate) in bDates.enumerated() {
+                (iXAxisValue, iYValue, iRawValue) = weekParser(dateType, i, iDate, rawItems)
+                
+                xAxisValues.append(iXAxisValue)
+                yValues.append(iYValue)
+                rawValues.append(iRawValue)
+            }
+
+        case .month:
+//            xLabelCount = 32
+
+            for (i, iDate) in bDates.enumerated() {
+                (iXAxisValue, iYValue, iRawValue) = monthParser(dateType, i, iDate, rawItems)
+
+                xAxisValues.append(iXAxisValue)
+                yValues.append(iYValue)
+                rawValues.append(iRawValue)
+            }
+
+        case .year:
+//            xLabelCount = 12
+
+            for (i, iDate) in bDates.enumerated() {
+                (iXAxisValue, iYValue, iRawValue) = yearParser(dateType, i, iDate, rawItems)
+                
+                xAxisValues.append(iXAxisValue)
+                yValues.append(iYValue)
+                rawValues.append(iRawValue)
+            }
+        }
+        
+        let (max, granularity, yAxisValues) = yAxisHandler(yValues, yAxisLabelCount)
+        retModel.max = max
+        retModel.granularity = granularity
+        retModel.xLabelCount = xLabelCount
+        retModel.xAxisValues = xAxisValues
+        retModel.yAxisValues = yAxisValues
+        retModel.yValues = yValues
+        retModel.rawValues = rawValues
+        
+        return retModel
+    }
+    
 }
 
 
@@ -192,6 +367,33 @@ extension XWHHealthyChartDataHandler {
         }
         
         return retArray
+    }
+    
+    private class func getDayXAxisValue(i: Int, iDate: Date) -> String {
+        return ""
+    }
+    
+    private class func getWeekXAxisValue(i: Int, iDate: Date) -> String {
+        return "\(iDate.month)/\(iDate.day)"
+    }
+    
+    private class func getMonthXAxisValue(i: Int, iDate: Date) -> String {
+        let cI = i + 1
+        if cI == 1 {
+            return iDate.localizedString(withFormat: XWHDate.monthDayFormat)
+        } else if cI % 7 == 0 {
+            return iDate.localizedString(withFormat: XWHDate.dayFormat)
+        } else {
+            return ""
+        }
+    }
+    
+    private class func getYearXAxisValue(i: Int, iDate: Date) -> String {
+        if i == 0 {
+            return iDate.localizedString(withFormat: XWHDate.monthFormat)
+        } else {
+            return (i + 1).string
+        }
     }
     
 }
