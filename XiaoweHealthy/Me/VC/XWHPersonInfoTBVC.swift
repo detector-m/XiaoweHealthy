80275a99e10b226a48db74af881ed3c1771a2dc8
Riven <lrw90316@163.com>
2022-07-19 09:55:20 +0800

    调整用户设置和用户基本信息样式

--- XiaoweHealthy/Me/VC/XWHPersonInfoTBVC.swift
+++ XiaoweHealthy/Me/VC/XWHPersonInfoTBVC.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class XWHPersonInfoTBVC: XWHTableViewBaseVC {
     
+    override var largeTitleWidth: CGFloat {
+        UIScreen.main.bounds.width - 32
+    }
+    
     override var topContentInset: CGFloat {
         66
     }
     
     override var titleText: String {
-        R.string.xwhDisplayText.个人资料()
+        R.string.xwhDisplayText.基本信息()
     }
     
     lazy var tbFooter = XWHTBButtonFooter(frame: CGRect(origin: .zero, size: CGSize(width: UIScreen.main.bounds.width, height: 160)))
@@ -36,25 +40,34 @@ class XWHPersonInfoTBVC: XWHTableViewBaseVC {
     
     override func setupNavigationItems() {
         super.setupNavigationItems()
-        
-        navigationItem.title = titleText
     }
     
-//    override func setNavigationBarWithLargeTitle() {
-//        setNav(color: .white)
-//        navigationItem.title = titleText
+    override func setNavigationBarWithLargeTitle() {
+        setNav(color: .white)
+        
+//        let leftItem = getNavItem(text: titleText, font: XWHFont.harmonyOSSans(ofSize: 16, weight: .medium), image: nil, target: self, action: #selector(clickNavLeftItem))
+//        navigationItem.leftBarButtonItem = leftItem
+        
+        navigationItem.title = titleText
+        
 //        setNavHidden(false, animated: true, async: false)
-//    }
-//
-//    override func resetNavigationBarWithoutLargeTitle() {
-//        setNavTransparent()
-//        navigationItem.title = nil
+    }
+    
+    override func resetNavigationBarWithoutLargeTitle() {
+        setNavTransparent()
+        
+        navigationItem.title = ""
+        
 //        setNavHidden(true, animated: true, async: false)
-//    }
+    }
     
     override func addSubViews() {
         super.addSubViews()
 
+        setLargeTitleMode()
+
+        largeTitleView.titleLb.text = titleText
+        
         tableView.tableFooterView = tbFooter
         tbFooter.button.setTitle(R.string.xwhDisplayText.保存(), for: .normal)
         DispatchQueue.main.async { [unowned self] in
@@ -66,10 +79,30 @@ class XWHPersonInfoTBVC: XWHTableViewBaseVC {
     }
     
     override func relayoutSubViews() {
-        tableView.snp.makeConstraints { make in
-            make.left.right.equalToSuperview()
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        tableView.snp.remakeConstraints { make in
+            make.left.right.equalToSuperview().inset(16)
+            make.top.equalToSuperview()
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+        
+        relayoutLargeTitle()
+        relayoutLargeTitleContentView()
+    }
+    
+    override func relayoutLargeTitleContentView() {
+        largeTitleView.relayout { ltView in
+            ltView.button.snp.remakeConstraints { make in
+                make.right.equalToSuperview().inset(12)
+                make.size.equalTo(24)
+                make.centerY.equalTo(ltView.titleLb)
+            }
+
+            ltView.titleLb.snp.remakeConstraints { make in
+                make.top.equalToSuperview()
+                make.height.equalTo(40)
+                make.left.equalToSuperview().inset(12)
+                make.right.lessThanOrEqualTo(ltView.button.snp.left).offset(-10)
+            }
         }
     }
     
